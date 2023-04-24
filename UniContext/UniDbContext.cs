f8e21a8084fc5ed6e948454274fa22405792524a
parnianRaf <parnian.rafie58@gmail.com>
2023-04-24 12:19:33 +0330

    first commit

---
+++ UniContext/UniDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AppCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace UniContext;
+public class UniDbContext:DbContext
+{
+    public UniDbContext(DbContextOptions<UniDbContext> options):base(options) { }
+   
+
+
+    DbSet<Course> Course { get; set; }
+    DbSet<Student> Student { get; set; }
+    DbSet<StudentContactInfo> StudentContactInfo { get; set; }
+    DbSet<StudentCourse> StudentCourse { get; set; }
+    DbSet<Teacher> Teacher { get; set; }
+    DbSet<TeacherContactInfo> TeacherContactInfo { get; set; }
+}
+
